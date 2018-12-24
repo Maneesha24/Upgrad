f87bd05ebf0bdf579bb023a3293a18c53509f235
Maneesha Venigalla <maneesha@janaspandana.in>
2018-12-24 23:26:15 +0530

    Implement add user module

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,203 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="landing container" id="main">
+      <header class="row d-flex justify-content-center mt-4">
+        <div class="col-md-3 text-right">
+          <img
+            src="https://ci6.googleusercontent.com/proxy/uPh3kp9eh0-YSNeguv9IKX71MGh40hdwIe-iImMQHQgiESxN3Ur6Z1Socgf2lR33ZuZORLHpoQ=s0-d-e1-ft#https://images.upgrad.com/upgrad.png"
+            alt
+          >
+        </div>
+        <div class="col-md-5">
+          <h3>ASSIGNMENT</h3>
+        </div>
+      </header>
+
+      <main class="row text-center mt-5 pl-3 mr-1">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your search"
+            aria-describedby="button-addon2"
+            v-model="search"
+          >
+          <div class="input-group-append">
+            <button
+              class="btn btn-primary"
+              type="button"
+              id="button-addon2"
+              @click="onSubmit"
+            >Add User</button>
+          </div>
+        </div>
+      </main>
+
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Employee_Id</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in filteredUsers" :key="user.name">
+              <td>{{user.id}}</td>
+              <td>{{user.firstName}}</td>
+              <td>{{user.lastName}}</td>
+              <td>{{user.email}}</td>
+              <td>{{user.phone}}</td>
+              <td><input type="checkbox" @click="showActiveModalFunc"/></td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+    <addUser v-show="show" id="addModal" @userData="onUserDataSubmit"/>
+    <activeUser v-show="showActiveModal" id="activeModal" />
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addUser from "./components/addUser.vue";
+import activeUser from "./components/activeUser.vue";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    addUser,
+    activeUser
+  },
+  data() {
+    return {
+      show: false,
+      showActiveModal: false,
+      users: [],
+      search: "",
+      closedModal: ""
+    };
+  },
+  created(){
+    this.users.push({
+      id: '1',
+      firstName: 'Maneesha',
+      lastName: 'venigalla',
+      email: 'venigallamaneesha24@gmail.com',
+      phone:'9398231139'
+    })
+
+  },
+  methods: {
+    onSubmit() {
+      this.show = !this.show;
+      document.getElementById("main").style.opacity = "0.3";
+    },
+    onUserDataSubmit(value) {
+      this.users.push({
+        id: value.id,
+        firstName: value.firstName,
+        lastName: value.lastName,
+        email: value.email,
+        phone: value.phone
+      });
+      this.show = false;
+      document.getElementById("main").style.opacity = "1";
+    },
+    onCloseModal(values){
+      this.closedModal = values;
+      console.log(values)
+
+    },
+    showActiveModalFunc(){
+      this.showActiveModal = !this.showActiveModal;
+       document.getElementById("main").style.opacity = "0.3";
+    }
+  },
+  computed: {
+    filteredUsers() {
+      return this.users.filter(user => {
+        return (
+          user.email.toLowerCase().indexOf(this.search.toLowerCase()) > -1 ||
+          user.firstName.indexOf(this.search.toLowerCase()) > -1 ||
+          user.lastName.indexOf(this.search.toLowerCase()) > -1 ||
+          user.id.indexOf(this.search.toLowerCase()) > -1 ||
+          user.phone.indexOf(this.search.toLowerCase()) > -1
+        );
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#addModal {
+  position: absolute;
+  top: 10%;
+  left: 30%;
+  z-index: 10;
+  border: 2px solid #777;
+  box-shadow: 0px 0px 1px rgba(102, 102, 102, 0.568);
+}
+
+
+#activeModal{
+   position: absolute;
+  top: 10%;
+  left: 30%;
+  z-index: 10;
+  border: 2px solid #777;
+    box-shadow: 0px 0px 1px rgba(102, 102, 102, 0.568);
+
+}
+
+
+
+table {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 1rem;
+  background-color: transparent;
+  border-collapse: collapse;
+}
+
+table thead th {
+  border-top: none;
+  border-bottom: none;
+  font-weight: 600;
+  text-transform: uppercase;
+  vertical-align: bottom;
+  font-size: 0.875rem;
+  text-align: left;
+}
+
+table thead {
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+}
+table td,
+table th {
+  padding: 1rem;
+  vertical-align: top;
+  border-top: 1px solid #e4e7ed;
+}
+
+table tbody td {
+  font-size: 1rem;
+  padding-bottom: 0.9rem;
+  padding-top: 1rem;
+  text-align: left;
+}
+table tbody tr {
+  padding-bottom: 1rem;
+}
+.j-table.table-striped tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.02);
 }
 </style>
+
---
+++ src/components/addUser.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="card" style="width: 35rem;" id="addModal">
+    <div class="card-header d-flex justify-content-between">
+     <div>Add user</div>
+     <div @click="closeModal">*</div>
+    </div>
+    <div class="card-content mt-3 mr-5 pb-3">
+      <form action @submit="handleSubmit">
+         <div class="input-group mb-3 ml-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="basic-addon1">
+                    <i class="fas fa-fingerprint"></i>
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Enter Employee ID"
+                  name="employeeID"
+                  class="form-control"
+                  v-model="user.id"
+                  required
+                >
+              </div>
+
+        <div class="input-group mb-3 ml-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">
+              <i class="fas fa-signature"></i>
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="Enter first name"
+            class="form-control"
+            name="firstName"
+            v-model="user.firstName"
+            required
+          >
+        </div>
+        <div class="input-group mb-3 ml-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">
+              <i class="fas fa-file-signature"></i>
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="Enter last name"
+            name="lastName"
+            class="form-control"
+            v-model="user.lastName"
+            required
+          >
+        </div>
+        <div class="input-group mb-3 ml-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">
+              <i class="fas fa-envelope"></i>
+            </span>
+          </div>
+          <input
+            type="text"
+            placeholder="Enter email"
+            id="email"
+            name="email"
+            class="form-control form-control-warning"
+            v-model="user.email"
+            required
+          >
+        </div>
+
+        <div class="input-group mb-3 ml-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="basic-addon1">
+                    <i class="fas fa-phone"></i>
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Enter mobile number"
+                  name="number"
+                  class="form-control"
+                  v-model="user.phone"
+                  required
+                >
+              </div>
+
+        <div class="input-group mb-3 ml-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fas fa-battery-three-quarters"></i>
+                </span>
+              </div>
+              <select
+                class="custom-select"
+                v-model="user.status"
+                required
+              >
+                <!-- <option selected>Status</option> -->
+                <option selected value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </select>
+            </div>
+            <div class="text-right">
+               <button type = "button" class="btn btn-secondary" @click="closeModal">Close</button>
+               <button class="btn btn-primary ml-3">Save user</button>
+            </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "activeUser",
+  data() {
+    return {
+      user: {
+        id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        status: "active",
+        closeModal: false
+      }
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      event.preventDefault();
+      this.$emit("userData", this.user);
+      this.user.id = "";
+      this.user.firstName = "";
+      this.user.lastName = "";
+      this.user.email = "";
+      this.user.phone = "";
+    },
+    closeModal(){
+      this.$emit("closeModal", this.closeModal)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
